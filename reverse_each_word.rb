b61a90cb8e2ac15e658c94a3a5719a9ec839fafa
Ben Altman <altmanguitar@gmail.com>
2020-11-10 23:14:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,8 +9,14 @@ def reverse_each_word(string)
 end
 =end
 
+=begin
 def reverse_each_word(string)
   array = string.split(/ /)
   new_array = array.collect {|word| word.reverse}
   new_string = new_array.join(" ")
+end
+=end
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
