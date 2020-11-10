607dc174c3f6ca15e638c46604ece05727a292eb
Ben Altman <altmanguitar@gmail.com>
2020-11-10 19:57:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   array = string.split(/ /)
   array.map do |word|
-    word.reverse
+    new_string = << word.reverse
+    
   end
   new_string = array.join(" ")
 end
