036a7f735f4435593073ad409ba51761328cbdba
Ben Altman <altmanguitar@gmail.com>
2020-11-10 21:11:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,17 @@
+=begin
 def reverse_each_word(string)
   array = string.split(/ /)
   new_array = []
   array.collect do |word|
   new_array << word.reverse
+  end
+  new_string = new_array.join(" ")
+end
+=end
+
+def reverse_each_word(string)
+  array = string.split(/ /)
+  new_array = array.collect do |word.reverse|
   end
   new_string = new_array.join(" ")
 end
